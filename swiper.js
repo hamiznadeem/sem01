6f0067c2fa6e03ab4cfb42ae8638f60e642d4757
Hamiz Nadeem <hamiznadeem137@gmail.com>
2025-05-07 12:54:16 +0500

    complete

---
+++ swiper.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+},
+});
